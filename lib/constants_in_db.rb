002ddbe7948125280e2dc7bf0250416e8f7f0597
= <nicolas.papon@webflows.fr>
2010-08-30 20:58:50 +0200

    rrh

--- lib/constants_in_db.rb
+++ lib/constants_in_db.rb
@@ -1,5 +1,5 @@
-require "constants_in_db/cache_constants"
-require "constants_in_db/seeds"
-require "constants_in_db/seeds_file"
+require File.expand_path(File.dirname(__FILE__)) + "/constants_in_db/cache_constants"
+require File.expand_path(File.dirname(__FILE__)) + "/constants_in_db/seeds"
+require File.expand_path(File.dirname(__FILE__)) + "/constants_in_db/seeds_file"
 
 ActiveRecord::Base.send(:extend, ConstantsCache)
